cf0772222c0a80bf309cabf5de13e4a26d8c52a2
Marshall Mohror <mohror64@gmail.com>
2025-07-27 15:45:53 -0500

    Attempt to add mobile support

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
-async function pageActionListener(tab: browser.tabs.Tab, info?: browser.pageAction.OnClickData) {
+async function toggleIncognitoDesktop(tab: browser.tabs.Tab) {
     const windows = await browser.windows.getAll();
     let window = windows.find((window) => window.focused && window.incognito != tab.incognito);
     if (!window) {
@@ -22,4 +22,44 @@ async function pageActionListener(tab: browser.tabs.Tab, info?: browser.pageActi
     await browser.tabs.remove(tab.id!);
 }
 
-browser.pageAction.onClicked.addListener(pageActionListener);
+async function toggleIncognitoMobile(tab: browser.tabs.Tab) {
+    if (tab.incognito) {
+        await browser.tabs.create({
+            // Oddly this creates a non-incognito tab,
+            // but still in the "private" section of the tab switcher
+            // window id had no effect.
+            active: true,
+            url: tab.url,
+        });
+        await browser.tabs.remove(tab.id!);
+    } else {
+        // We can't make a new incognito tab. See if there's an empty incognito tab to hijack.
+        const newtabs = await browser.tabs.query({
+            url: ["about:newtab"]
+        });
+        const incognitoTab = newtabs.find(tab => tab.incognito);
+        if (!incognitoTab) {
+            throw new Error("No new incognito tab to use")
+        }
+        await browser.tabs.update(incognitoTab.id!, {
+            active: true,
+            url: tab.url
+        })
+        await browser.tabs.remove(tab.id!);
+    }
+}
+
+async function actionListener(tab: browser.tabs.Tab, info?: browser.pageAction.OnClickData) {
+    try {
+        if (browser.windows) {
+            await toggleIncognitoDesktop(tab);
+        } else {
+            await toggleIncognitoMobile(tab);
+        }
+    } catch (e) {
+        console.error(`Failed to toggle incognito:\n${JSON.stringify(e)}`);
+        throw e;
+    }
+}
+
+browser.action.onClicked.addListener(actionListener)
